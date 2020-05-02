2826f9b21659ed6b396cc44408d2becacd48effa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 18:26:12 +0000

    Deploy Arquisoft/viade_es1c to github.com/Arquisoft/viade_es1c.git:gh-pages

---
+++ precache-manifest.cebc76426cb03f48c44cd88284ebbcfa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d875437d5988027fc0741d73c917094c",
+    "url": "/viade_es1c/index.html"
+  },
+  {
+    "revision": "63dde42465222dc3c762",
+    "url": "/viade_es1c/static/css/2.94f2424c.chunk.css"
+  },
+  {
+    "revision": "4299874ef2bcf58e0203",
+    "url": "/viade_es1c/static/css/main.d727d704.chunk.css"
+  },
+  {
+    "revision": "63dde42465222dc3c762",
+    "url": "/viade_es1c/static/js/2.aa88378f.chunk.js"
+  },
+  {
+    "revision": "4299874ef2bcf58e0203",
+    "url": "/viade_es1c/static/js/main.03b9444c.chunk.js"
+  },
+  {
+    "revision": "30346b847985ba504ec5",
+    "url": "/viade_es1c/static/js/runtime-main.b86410ad.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/viade_es1c/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/viade_es1c/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/viade_es1c/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/viade_es1c/static/media/notification.c0d3c94c.eot"
+  }
+]);
